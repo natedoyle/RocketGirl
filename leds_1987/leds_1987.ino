0a5d0b41b648fcb1e0fa1b94f90a2493236b039a
Nathan <nathan@KevansMacbook.local>
2016-04-03 14:15:31 -0600

    initial commit to Rocket Girl LED project.

---
+++ leds_1987/leds_1987.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  // turn on some lights, or don't. It doesn't matter to me.
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
